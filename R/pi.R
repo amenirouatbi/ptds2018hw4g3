d2127e1560e6347c7e6b1507011fb8514ba5e099
BartRoes <bart.roes@unil.chgit config --global user.email bart.roes@unil.ch>
2018-11-27 16:09:40 +0100

    Including documentation for estimate pi2 function (#2)

--- R/pi.R
+++ R/pi.R
@@ -96,7 +96,29 @@ plot.pi <- function(x) {
 }
 
 
-
+#' @title Estimate Pi from random sampling with Rcpp integration
+#'
+#' @description Compute an approximation of pi by drawing x and y coordinates
+#' from a uniform distribution and checking whether or not they fall inside a
+#' unit circle.
+#' @param B A \code{numeric} (integer) used to denote the number of sample
+#' coordinates used to approximate pi
+#' @param seed A \code{numeric} used to control the seed of the random number
+#' generator used by this function.
+#' @return A \code{list} containing the following attributes:
+#' \describe{
+#'      \item{estimated_pi}{Estimated value of pi}
+#'      \item{points}{Dataframe of the generated coordinates with an indication
+#'      whether or not the coordinate is inside the circle}
+#' }
+#' @author Vanessa Armistead
+#' @author Bart Roes
+#' @author Luisa Pricken
+#' @author Ameni Rouatbi
+#' @importFrom stats runif
+#' @export
+#' @examples
+#' estimate_pi2(B=10)
 
 estimate_pi2 <- function(B = 5000, seed = 10){
     # Control seed
