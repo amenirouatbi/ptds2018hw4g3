141c2327a5e474462264fe3ceecd81768953a511
luisapricken <luisa.prickenbrandao@unil.ch>
2018-11-29 16:25:51 +0100

    created file to add documentation of runDemo function using roxygen2

--- R/runDemo.R
+++ R/runDemo.R
@@ -4,11 +4,14 @@ runDemo <- function() {
     appDir <- system.file("shiny-examples", "pi", package = "ptds2018hw4g3")
     if (appDir == "") {
         stop(
-            "Could not find example directory. Try re-installing ptds2018hw4g3.",
-            call. = FALSE
+            "Could not find example directory. Try re-installing ptds2018hw4g3."
+            , call. = FALSE
         )
     }
 
     shiny::runApp(appDir, display.mode = "normal")
 
 }
+
+devtools::document()
+
