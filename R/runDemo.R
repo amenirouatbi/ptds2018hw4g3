1dc5ce21ce30a742521e696f91b195cb5c98692a
luisapricken <luisa.prickenbrandao@unil.ch>
2018-11-29 15:54:07 +0100

    add file runDemo (#3)

---
+++ R/runDemo.R
@@ -0,0 +1,14 @@
+#' @export
+runDemo <- function() {
+
+    appDir <- system.file("shiny-examples", "pi", package = "ptds2018hw4g3")
+    if (appDir == "") {
+        stop(
+            "Could not find example directory. Try re-installing ptds2018hw4g3.",
+            call. = FALSE
+        )
+    }
+
+    shiny::runApp(appDir, display.mode = "normal")
+
+}
